3816e7b8b3520f27e547c9a243daff320aadd45e
Maria Lotts <marialotts@MacBook-Air-Maria.local>
2023-11-08 17:27:36 +0600

    initial

---
+++ selenium/specs/first.spec.js
@@ -0,0 +1,37 @@
+const expect = require('chai').expect
+
+describe('First suit', function (){
+
+    it('first test', async function(){
+        await browser.get('https://www.encuentralohoy.com/dev/offer/bv-2-sso-u')
+      
+      
+
+        // const searchValue = 'телефон samsung'
+
+        // const searchInputSelector = 'body > app-root > div > div > rz-header > rz-main-header > header > div > div > div > form > div > div.search-form__input-wrapper.search-form__input-wrapper_state_active > input'
+        // const searchInput = $(searchInputSelector)
+
+        // const searchButtonSelector = 'body > app-root > div > div > rz-header > rz-main-header > header > div > div > div > form > button'
+        // const searchButton = $(searchButtonSelector)
+
+        // const catalogTitleSelector = 'body > app-root > div > div > rz-category > div > main > div:nth-child(1) > div > h1'
+        // const catalogTitle = $(catalogTitleSelector)
+
+    //     await browser.get('https://rozetka.com.ua/')
+    //     await browser.wait(EC.visibilityOf(searchInput), 5000, 'Search input should be visible')
+    //     await browser.wait(EC.visibilityOf(searchButton), 5000, 'Search button should be visible')
+
+    //     await searchInput.getText(searchValue)
+    //     await searchButton.click()
+
+    //     await browser.wait(EC.visibilityOf(catalogTitle), 15000, 'Catalog title should be visible')
+
+    //     const catalogTitleValue = await catalogTitle.getText()
+
+    //     // expect(catalogTitleValue.toLowerCase()).to.include('samsung')
+
+        
+
+    })
+})
